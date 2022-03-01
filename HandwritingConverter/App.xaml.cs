a19f32365e43ad5bdae714049963456d956749f3
Aidan Cheung <aidan.cheung@outlook.com>
2022-03-01 09:00:42 +0000

    Fixed navigation

--- HandwritingConverter/App.xaml.cs
+++ HandwritingConverter/App.xaml.cs
@@ -100,7 +100,7 @@ namespace HandwritingConverter
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(ConverterPage), e.Arguments);
+                    rootFrame.Navigate(typeof(NavigationPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
