69be325b47042f845fc4622205db6791122691ce
Aidan Cheung <aidan.cheung@outlook.com>
2022-03-17 12:05:25 +0000

    Removed unecessary using lines

--- HandwritingConverter/NotesPage.xaml.cs
+++ HandwritingConverter/NotesPage.xaml.cs
@@ -1,27 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.UI.Input;
-using Windows.UI.Input.Inking;
-using Windows.UI.Input.Inking.Analysis;
-using Windows.Storage.Streams;
-using Windows.UI.ViewManagement;
-using Windows.UI;
-using Windows.ApplicationModel.Core;
-using muxc = Microsoft.UI.Xaml.Controls;
-using Windows.UI.Core;
-using Windows.System;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 
 namespace HandwritingConverter
 {
