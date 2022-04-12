94e76c899bf0f5f9fd1eb34a2e57428f1bebf0f7
Aidan Cheung <aidan.cheung@outlook.com>
2022-04-12 09:56:51 +0100

    Removed unecessary disclaimer

--- HandwritingConverter/SettingsPage.xaml.cs
+++ HandwritingConverter/SettingsPage.xaml.cs
@@ -6,12 +6,6 @@ using Windows.UI.Xaml;
 
 namespace HandwritingConverter
 {
-    /*
-    The following code uses modified samples from the Microsoft UWP Documentation, 
-    these samples are taken from the following pages granted under the MIT license:
-
-    - https://docs.microsoft.com/en-us/windows/apps/design/controls/navigationview
-    */
     public sealed partial class SettingsPage : Page
     {
         public SettingsPage()
@@ -19,7 +13,6 @@ namespace HandwritingConverter
             this.InitializeComponent();
 
             string appVersion = $"Handwriting Converter {Package.Current.Id.Version.Major}.{Package.Current.Id.Version.Minor}.{Package.Current.Id.Version.Build} Beta";
-
             versiontext.Text = appVersion;
         }
 
